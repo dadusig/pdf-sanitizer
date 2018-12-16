cbd0393cf2927bf1e0d679cef3389e0ceb0a580c
Damianos Dumi Sigalas <nsigalas@ceid.upatras.gr>
2018-12-16 18:02:44 +0200

    Create dictionary of files

--- sanitizer.py
+++ sanitizer.py
@@ -2,7 +2,7 @@
 import os
 from os import path
 from glob import glob
-import sys
+import sys, random
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from PyPDF2.generic import NameObject, createStringObject
 
@@ -10,70 +10,29 @@ from PyPDF2.generic import NameObject, createStringObject
 def find_ext(dr, ext):
     return glob(path.join(dr,"*.{}".format(ext)))
 
+def makeList(path):
+	inputPdf = PdfFileReader(open(path, "rb"), strict=False)
+	docInfo = inputPdf.getDocumentInfo()
+	return [path, os.path.basename(path), docInfo.title, docInfo.author, docInfo.subject, docInfo.producer, docInfo.creator]
+
 def main():
-	# path of all pdf all_pdf_files
-	# very bad implemenation. todo: use getopt
-	my_path="." #default path is the current folder
-	if len(sys.argv) == 2:
-		my_path=sys.argv[1]
-	elif len(sys.argv) > 2:
-		print("Something went wrong. Check your arguments.")
-		exit(-1)
+	my_path="./input_files"
+	# N = random.randint(100,200)
+	N = 100
 
-	# create results directory if not exists
-	os.makedirs(my_path + "/results" , exist_ok=True)
-
-	print("Path given: ", my_path, "\n")
-
-	# get all pdf files from specified folder
 	all_pdf_files = find_ext(my_path,"pdf")
+	K = int(len(all_pdf_files))
 
+	filesDict = {}
 
+	for i in range(N, N+K):
+		filesDict[i] = makeList(all_pdf_files[i-N])
 
-	c = 0
-	for pdf in all_pdf_files:
-		inputPdf = PdfFileReader(open(pdf, "rb"), strict=False) # strict=False for Windows support - PdfReadWarning: Superfluous whitespace found in object header
-		docInfo = inputPdf.getDocumentInfo()
+	# print(filesDict)
 
-		# create metadata patch
-		output = PdfFileWriter()
-		infoDict = output._info.getObject()
-		infoDict.update({
-			NameObject('/Title'): createStringObject(u'title removed'),
-			NameObject('/Author'): createStringObject(u'author removed'),
-			NameObject('/Subject'): createStringObject(u'subject removed'),
-			NameObject('/Creator'): createStringObject(u'software quality script'),
-			NameObject('/Producer'): createStringObject(u'software quality script'),
-			NameObject('/Keywords'): createStringObject(u'software, quality, sanitized')
-		})
+	for i in range(N, N+K):
+		print(i, filesDict[i][1])
 
-		c += 1
-
-		# get filename from path
-		filename = os.path.basename(pdf)
-		# remove the extension
-		filename_noext = os.path.splitext(filename)[0]
-
-		#print some information
-		print(c, ". Processing file: ", pdf)
-		print("filename: ", filename)
-		print("Title: ", docInfo.title)
-		print("Author: ", docInfo.author)
-		print("Subject: ", docInfo.subject)
-		print("Producer: ", docInfo.producer)
-		print("Creator: ", docInfo.creator)
-
-		#create new pdf
-		for page in range(inputPdf.getNumPages()):
-			output.addPage(inputPdf.getPage(page))
-
-		print("\n")
-
-		# write new file in results subfolder
-		# the new file name will be the old one extended with "-sanitized.pdf"
-		outputStream = open(my_path+"/results/"+filename_noext+"-sanitized.pdf", 'wb')
-		output.write(outputStream)
-		outputStream.close()
-
+	
 if __name__ == '__main__':
     main()
